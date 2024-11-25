d9cd8d5b49fdadc4ca24ab32d4a4695c7b553dbc
Rizvan85 <anfomindia@gmail.com>
2024-11-25 13:35:50 +0530

    all chng done

---
+++ index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <script src="js/typed.umd.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+        <a href="#"><h1>Portfolio</h1></a>
+       </div>
+
+       <div class="navbar">
+         <nav>
+            <ul>
+                <li><a href="#home" style="--1:1;" class="active">Home</a></li>
+                <li><a href="#about" style="--1:2;" >About</a></li>
+                <li><a href="#service" style="--1:3;" >Skill</a></li>
+                <li><a href="#portfolio" style="--1:4;" >Portfolio</a></li>
+                <li><a href="#contact" style="--1:5;" >Contact</a></li>
+                
+
+            </ul>
+        </nav>
+        <button class="mobilebtn">Menu</button>
+      </div>
+
+    </header>
+    <main id="contentM">
+    <section class="home" id="home">
+        <div class="home-content">
+            <div class="text">
+            <h3>Hello, It's Me</h3>
+            <h1>Mohammad Rizvan</h1>
+            <h3>And I'm a &nbsp;&nbsp; <span id="text"></span></h3>
+            <p> I am a Web Designer with extensive experiene for over 4 year.
+                <br/>expertise is to create and website design,Frontend design ,
+                and many more...</p>
+            </p>
+           
+            <div class="socialIcon">
+                <a href="#" style="--1:6;" ><box-icon type='logo' name='facebook-circle' title="Facebook" color="#59ece4"></box-icon></a>
+                <a href="#" style="--1:7;" ><box-icon type='logo' name='twitter' style="color:white" color="#59ece4" title="twitter"></box-icon></a>
+                <a href="#" style="--1:8;" ><box-icon type='logo' name='linkedin-square' title="linkedin" color="#59ece4"></box-icon></a>
+                <a href="#" style="--1:9;" ><box-icon type='logo' name='youtube' title="Youtube" color="#59ece4"></box-icon></a>
+                <a href="#" style="--1:10;" ><box-icon type='logo' name='instagram-alt' color="#59ece4" title="instagram"></box-icon></a>
+                <a href="#" style="--1:11;" ><box-icon type='logo' name='whatsapp-square' color="#59ece4" title="whatsapp"></box-icon></a>
+              </div>
+            <a href="" class="btnBox">More About Me</a>
+          
+</div>
+<div class="home-sci">
+    <div class="img">
+       <img src="./image/prof1.png" class="img"/>
+   </div>
+
+</div>
+        </div>
+    </section>
+    <section class="about" id="about">
+        <div class="aboutImg">
+     
+       <img src="./image/prof1.png" class="img"/>
+
+        </div>
+        <div class="aboutText" id="about">
+            <h2>About <span>Me</span></h2>
+            <h4>Web Developer</h4>
+            <p>I am a skilled web designer with over 3 years of experience in the industry.My passion lies in   
+creating captivating website design and implementing them through frontend development.
+i take price in staying up to date with curent design trends and leveraging my creativity to produce 
+visually appealing and user friendly websites.Throughout my career, i have developed a deep understanding
+of user experience and user interface principles,
+By puling myself in the shoes of the end users,I strive to create intutitive and seamless.
+browsing experiences. i believe a smooth and enjoyable interacton for visitors.when starting a new 
+project,i throughly research and analyze the larget audience and the client's specific requirements,
+This enables me to tailor my designs to meet their expectations and deliver a unique online presence
+that aligns with their brand identity.</p>
+                 <a href="" class="btn-box">Read More...</a>
+        </div>
+
+    </section>
+    <section>
+        <div class="service">
+        
+            <div class="container">
+            <h1 class="subTitle">My <span>Service</span></h1>
+                <div class="serviceList">
+                    <div>
+                   <h2> <box-icon name='code' color='#0ef'></box-icon></h2>
+                         <h2>UI/Ux Developer </h2>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                          Enim in quibusdam sed totam ipsa! Ullam a minima consequatur cum iure, 
+                          provident mollitia accusamus commodi?</p>
+                          <a href="#" class="read">Learn More...</a>
+                    </div>
+                    <div>
+                    <box-icon name='file-html' type='solid' color='#00eeff' ></box-icon>
+                         <h2>Html Developer</h2>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                          Enim in quibusdam sed totam ipsa! Ullam a minima consequatur cum iure, 
+                          provident mollitia accusamus commodi?</p>
+                          <a href="#" class="read">Learn More...</a>
+                    </div>
+                    <div>
+                    <box-icon type='logo' name='angular' color='#00eeff'></box-icon>
+                         <h2>Angular Developer</h2>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                          Enim in quibusdam sed totam ipsa! Ullam a minima consequatur cum iure, 
+                          provident mollitia accusamus commodi?</p>
+                          <a href="#" class="read">Learn More...</a>
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+      
+    </section>  
+    <h2 class="subTitle">My<span>Skills</span></h2>
+    <section id="service">
+        <div class="sectionBar">
+         <div class="container1">
+            <div class="skill" id="skill">
+                   <h1 class="heading1">Technical Skill</h1>
+                    <div class="technicalBar">
+                        <div class="bar" style="color:#c95d2e">
+                        <box-icon type='logo' name='html5' color='#c95d2e'></box-icon>
+                           <div class="Info">
+                              <span>Html</span>
+                            </div>
+                             <div class="progressLine Html">
+                                   <span></span>
+                               </div>
+                         </div>
+
+                         <div class="bar" style="color:#147bbc">
+                         <box-icon name='file-css' type='solid' color='#147bbc' ></box-icon>
+                           <div class="Info">
+                              <span>Css</span>
+                            </div>
+                             <div class="progressLine css">
+                                   <span></span>
+                               </div>
+                         </div>
+                         <div class="bar" style="color:#b0bc1e">
+                         <box-icon type='logo' color='#b0bc1e' name='javascript'></box-icon>
+                           <div class="Info">
+                              <span>JavaScript</span>
+                            </div>
+                             <div class="progressLine javascript">
+                                   <span></span>
+                               </div>
+                         </div>
+                         <div class="bar">
+                         <box-icon type='solid' color='#0ef' name='photo-album'></box-icon>
+                           <div class="Info">
+                              <span>Photoshop</span>
+                            </div>
+                             <div class="progressLine photoshop">
+                                   <span></span>
+                               </div>
+                         </div>
+                         <div class="bar" style="color:#c32ec9">
+                         <box-icon type='logo' color='#c32ec9' name='php'></box-icon>
+                           <div class="Info">
+                              <span>PHP</span>
+                            </div>
+                             <div class="progressLine php">
+                                   <span></span>
+                               </div>
+                         </div>
+                         <div class="bar" style="color:#d6002f">
+                         <box-icon type='logo' color='#d6002f' name='angular'></box-icon>
+                           <div class="Info">
+                              <span>Angular</span>
+                            </div>
+                             <div class="progressLine angular">
+                                   <span></span>
+                               </div>
+                         </div>
+                         <div class="bar" style="color:#ec8b0d;">
+                         <box-icon type='solid' color='#59ece4' name='php'></box-icon>
+                           <div class="Info">
+                              <span>MySql</span>
+                            </div>
+                             <div class="progressLine mysql">
+                                   <span></span>
+                               </div>
+                         </div>
+
+                    </div>
+
+            </div>
+         </div>
+    <div class="container1">
+             <h1 class="heading1">Professional Skills</h1>
+         <div class="radialBars">
+            <div class="radialBar">
+                 <svg x="0px" y="0px" viewBox="0 0 200 200">
+                    <circle class="progressBar" cx="100" cy="100" r="80"></circle>
+                    <circle class="path path-1" cx="100" cy="100" r="80"></circle>
+                </svg>
+                  <div class="percentage">90%</div>
+                  <div class="text">Creativity</div>
+            </div>
+           <div class="radialBar">
+              <svg x="0px" y="0px" viewBox="0 0 200 200">
+                <circle class="progressBar" cx="100" cy="100" r="80"></circle>
+                 <circle class="path path-1" cx="100" cy="100" r="80"></circle>
+              </svg>
+                  <div class="percentage">65%</div>
+                  <div class="text">Comunication</div>
+            </div>
+            <div class="radialBar">
+              <svg x="0px" y="0px" viewBox="0 0 200 200">
+                <circle class="progressBar" cx="100" cy="100" r="80"></circle>
+                 <circle class="path path-1" cx="100" cy="100" r="80"></circle>
+              </svg>
+                  <div class="percentage">75%</div>
+                  <div class="text">Problem Solving</div>
+           </div>
+           <div class="radialBar">
+              <svg x="0px" y="0px" viewBox="0 0 200 200">
+                <circle class="progressBar" cx="100" cy="100" r="80"></circle>
+                 <circle class="path path-1" cx="100" cy="100" r="80"></circle>
+              </svg>
+                  <div class="percentage">85%</div>
+                  <div class="text">Team Work</div>
+            </div>
+      </div>
+
+  </div>
+</div>
+
+<section id="portfolio">
+    <div  class="portfolio">
+        <div class="main-text" id="project">
+        <h2 class="subTitle">Latest<span>Project</span></h2>
+        <div class="portfolio-content">
+        <div class="boxImg">
+                <img src="image/proj1.PNG">
+    
+               
+            <div class="content">
+                <h5>Anfotal Nutrition</h5>
+              <p>  This Web site is Design and developed by me using latest technologies
+                like Html CSS JAVASCRIPT PHOTOSHOP AND BOOTSTRAP
+              </p>
+                <a href="https://www.anfotalnutritions.com/">Website</a>
+            </div>
+               
+            </div>
+
+            <div class="boxImg">
+            <img src="image/projec3.PNG">
+           
+               
+            <div class="content">
+                <h5>Dynamic infotech</h5>
+                <p>  This Web site is Design and developed by me using latest technologies
+                like Html CSS JAVASCRIPT PHOTOSHOP AND BOOTSTRAP
+              </p>
+                <a href="http://dynamicinfotech.co.in/">website</a>
+            </div>
+               
+            </div>
+            <div class="boxImg">
+            <img src="image/projec2.PNG">
+                   <div class="content">
+                <h5>Mirza Interior</h5>
+                <p>  This Web site is Design and developed by me using latest technologies
+                like Html CSS JAVASCRIPT PHOTOSHOP AND BOOTSTRAP
+              </p>
+                <a href="https://rizvan85.github.io/mg-interior/">Website</a>
+            </div>
+               
+            </div>
+        </div>
+        </div>
+  
+</div>
+</section>
+<section class="contact" id="contact">
+    <div class="content-text">
+        <h2>Contact<span>Us</span></h2>
+        <h4>Let's Work Togather</h4>
+       <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, porro.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, porro.</p>
+        <div class="contact-list">
+            <ul>
+                <li><box-icon name='send' color='#00eeff' ></box-icon>&nbsp;mohammad.rizvan658@gmail.com</li>
+                <li> <box-icon name='phone' color='#00eeff'></box-icon> &nbsp;9958432811</li>
+            </ul>
+
+        </div>
+        <div class="contactIcon">
+            <a href="" title="facebook">
+                <box-icon type='logo' class="box-icon" color='#59ece4'  name='facebook-circle'></box-icon>
+            </a>
+            <a href="" title="Instagram">
+            <box-icon name='instagram' type='logo' color='#59ece4' class="box-icon"  ></box-icon>
+            </a>
+            <a href="" title="Twitter"><box-icon name='twitter' class="box-icon" color='#59ece4' type='logo'  ></box-icon></a>
+            <a href="" title="Linkedin"><box-icon type='logo' name='linkedin' color='#59ece4'  class="box-icon"></box-icon></a>
+            <a href="" title="Youtube"><box-icon type='logo' name='youtube' color='#59ece4'class="box-icon"></box-icon></a>
+        
+        </div>
+    </div>
+    <div class="contactForm">
+        <form action="">
+            <input type="text" placeholder="Enter Your Name" required>
+            <input type="email" placeholder="Enter your email" required>
+            <input type="Enter your Subject">
+            <textarea rows="10" cols="15" name="" id="" placeholder="Enter Your message" required></textarea>
+            <input type="submit" name="submit" value="submit" class="send">
+
+        </form>
+    </div>
+
+</section>
+
+
+ 
+
+
+
+</main>
+  
+<div class="designby">
+    <a href="#" class="top"><box-icon name='arrow-back' rotate='90' color='#26f9e5' ></box-icon></a>
+    <p>Developed by Mohammad Rizvan</p>
+
+</div>
+   
+
+    <script src="./js/style.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+</body>
+</html>
---
+++ js/style.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#text",{
+    strings: ['web developer', 'UI Developer', 'Front end Developer','web Designer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:3000,
+    loop:true
+});
+
+
